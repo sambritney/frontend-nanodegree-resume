85ba63ac58f45df8016aed898116e0f1738b633a
Samantha Britney <sambritney@Samanthas-MacBook-Air.local>
2016-03-22 23:15:46 -0700

    Added content, logo, images; changes to font, color in css

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var name = "Samantha Britney";
+var name = "Samantha A. Britney";
 var formattedName = HTMLheaderName.replace("%data%", name);
 
 var role = "Web Developer";
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
 
 var bio = {
 	"name" : name,
@@ -20,27 +21,45 @@ var bio = {
 		"github" : "sambritney",
 		"location" : "Carlsbad, CA"
 	},
-	"bioPic" : "images/fry.jpg",
-	"skills" : ["french", "cooking", "travel", "reading"]
+	"message" : "High tech professional with global expertise in User Experience, Product and Marketing. Demonstrated focus on improving the customer experience coupled with strong analytical, communication and problem solving skills. Principle strengths include empathy and adaptability. Accomplished in developing teams, directing, influencing and motivating others to deliver high quality products and services.",
+	"skills" : ["french", "cooking", "travel", "reading"],
+	"bioPic" : "images/logo_s_orange.png"
 };
 
-$("#header").append(bio);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+$("#topContacts").append(formattedTwitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#topContacts").append(formattedGithub);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#topContacts").append(formattedLocation);
+
+
+var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$("#header").prepend(formattedPic);
+var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.message);
+$("#header").append(formattedMessage);
+
 
 if (bio.skills.length > 0) {
 	$("#header").append(HTMLskillsStart);
 
 var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-$("#skills").append(formattedSkill);
+$("#header").append(formattedSkill);
 
 formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-$("#skills").append(formattedSkill);
+$("#header").append(formattedSkill);
 
 formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-$("#skills").append(formattedSkill);
+$("#header").append(formattedSkill);
 
 formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-$("#skills").append(formattedSkill);
+$("#header").append(formattedSkill);
 }
+
 
 var work = {
 	"jobs" : [
@@ -49,14 +68,42 @@ var work = {
 		"employer" : "Expedia, Inc.",
 		"dates" : "2014-2015",
 		"location" : "Bellevue, WA",
-		"description" : "Reporting and BI Product Manager for business travel site"
+		"description" : "Built strategic partnerships with global customers and teams to advance Business Intelligence (BI) product strategy. Acted as Scrum product owner for BI product offerings."
 	},
 	{
 		"title" : "Director, Product Management",
 		"employer" : "moz",
-		"dates" : "2011 - 2013",
+		"dates" : "2012-2013",
 		"location" : "Seattle, WA",
-		"description" : "Marketing analytics site"
+		"description" : "Drove product strategy, managed backlog and inspired product team for Marketing Analytics SaaS start-up."
+	},
+	{
+		"title" : "Senior Product Manager",
+		"employer" : "moz",
+		"dates" : "2011-2012",
+		"location" : "Seattle, WA",
+		"description" : "Conducted user research, developed product plans, wireframes and specifications. Led cross-functional teams through agile product development."
+	},
+	{
+		"title" : "Business Manager, Online Advertising",
+		"employer" : "Microsoft Corp.",
+		"dates" : "2008-2010",
+		"location" : "Redmond, WA",
+		"description" : "Drove operational business processes including strategic planning and business reviews for 50-person Global Marketing team. Responsible for financial management and analysis of $16M Marketing budget."
+	},
+	{
+		"title" : "Marketing Communcations Manager, Atlas",
+		"employer" : "Microsoft Corp.",
+		"dates" : "2008",
+		"location" : "Redmond, WA",
+		"description" : "Produced marketing collateral including brochures, product sheets, white papers and event signage. Reviewed outgoing Marketing Communications for brand accuracy."
+	},
+	{
+		"title" : "Program Manager, Dynamics",
+		"employer" : "Microsoft Corp.",
+		"dates" : "2002-2007",
+		"location" : "Redmond, WA",
+		"description" : "Remotely managed cross-functional teams designing features for Microsoft Dynamics financial management software. Developed use cases, technical specifications, wireframes and process models."
 	}
 	]
 };
@@ -78,7 +125,7 @@ for (job in work.jobs) {
 
 	var formattedDesc = HTMLworkDescription.replace("%data%", work.jobs[job].description);
 	$(".work-entry:last").append(formattedDesc);
-}
+	}
 }
 
 displayWork();
@@ -86,14 +133,10 @@ displayWork();
 var projects = {
 	"projects" : [
 	{
-			"title" : "Action List",
-			"dates" : "2012",
-			"description" : "Developed an action list to show users next steps in optimizing SEO"
-	},
-	{
-			"title" : "Historical Tracking",
-			"dates" : "2011",
-			"description" : "Added historical data points to rankings tracking"
+			"title" : "Placeholder for a Project",
+			"dates" : "TBD",
+			"description" : "This is a placeholder for where a project will go",
+			"images" : ["images/comb1_d.png", "images/comb3.png"]
 	}
 	]
 };
@@ -111,13 +154,14 @@ projects.display = function () {
 	var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
 		$(".project-entry:last").append(formattedDescription);
 
-	// if (projects.projects[project].images.length > 0) {
-	// 	for (image in projects.projects[project].images) {
-	// 		var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-	// 		$(".project-entry.last").append(formattedImage);
-	// 	}
-	// }
-}
+	if (projects.projects[project].images.length > 0) {
+		for (image in projects.projects[project].images) {
+			var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+			$(".project-entry:last").append(formattedImage);
+			}
+		}
+
+	}
 }
 
 projects.display();
@@ -128,15 +172,13 @@ var education = {
 		"name" : "University of Washington",
 		"degree" : "MBA",
 		"location" : "Seattle, WA",
-		"major" : "Technology Management",
-		"dates" : 2007
+		"major" : "Technology Management"
 	},
 	{
 		"name" : "Syracuse University",
 		"degree" : "BA",
 		"location" : "Syracuse, NY",
-		"major" : "English",
-		"dates" : 1993
+		"major" : "English"
 	}
 	]
 };
@@ -146,13 +188,9 @@ for (school in education.schools) {
 	$("#education").append(HTMLschoolStart);
 
 	var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
-		$(".education-entry:last").append(formattedName);	
-
 	var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-		$(".education-entry:last").append(formattedDegree);
-
-	var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-		$(".education-entry:last").append(formattedDates);
+	var formattedNameDegree = formattedName + formattedDegree;
+		$(".education-entry:last").append(formattedNameDegree);
 
 	var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
 		$(".education-entry:last").append(formattedLocation);
